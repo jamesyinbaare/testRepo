76adf34e669ff2075fae9163bfe0cb1bba88f11e
James Yin <james@savannahtech.io>
2023-11-09 10:22:53 +0000

    Update ci

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "world"}
+    assert response.json() == {"Hello": "worl"}
